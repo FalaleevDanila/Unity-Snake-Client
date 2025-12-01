a1c6678095192ace153733787d836d169bdae12a
FalaleevDanila <falaleevdanila@gmail.com>
2025-12-01 18:26:29 +0300

    add sending

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,14 +1,18 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
     [SerializeField] private Transform _cursor;
     Snake _snake;
+
+    private MultiplayerManager _multiplayerManager;
     private Camera _camera;
     private Plane _plane;
-    
+
     public void Init(Snake snake)
     {
+        _multiplayerManager = MultiplayerManager.Instance;
         _snake = snake;
         _camera = Camera.main;
         _plane = new Plane(Vector3.up, Vector3.zero);
@@ -30,5 +34,16 @@ public class Controller : MonoBehaviour
         Vector3 point = ray.GetPoint(distance);
 
         _cursor.position = point;
+    }
+
+    private void SendMove()
+    {
+        _snake.GetMoveInfo(out Vector3 position);
+        Dictionary<string, object> data = new Dictionary<string, object>()
+        {
+            {"x", position.x},
+            {"z", position.z}
+        };
+       _multiplayerManager.SendMessage("move", data);
     }
 }
